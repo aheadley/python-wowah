ff112bf67f71ee48f8b54bf98338b82fb6641df6
aheadley <aheadley@waysaboutstuff.com>
2016-10-27 00:57:31 -0400

    lot of type/code clean up, add progress bars

--- wowah.py
+++ wowah.py
@@ -10,6 +10,8 @@ import json
 
 # import peewee
 from peewee import *
+from playhouse.db_url import connect as db_connect
+from tqdm import tqdm
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -18,7 +20,7 @@ logging.getLogger('peewee').setLevel(logging.INFO)
 DB_PROXY = Proxy()
 
 class AuctionDataModel(Model):
-    guid                = UUIDField(default=uuid.uuid4, index=True, unique=True)
+    # guid                = UUIDField(default=uuid.uuid4, index=True, unique=True)
 
     class Meta:
         database        = DB_PROXY
@@ -28,14 +30,14 @@ class Auction(AuctionDataModel):
     owner               = CharField(default=u'???', index=True)
     owner_realm         = CharField(default=u'???')
 
-    quantity            = IntegerField()
+    quantity            = IntegerField(constraints=[Check('quantity > 0')])
     buyout              = IntegerField(default=0)
 
     item_id             = IntegerField(index=True)
     rand                = IntegerField(default=0)
     seed                = IntegerField(default=0)
     context             = IntegerField(default=0)
-    item_extra          = TextField(default='{}')
+    item_extra          = TextField(default=None, null=True)
 
     is_active           = BooleanField(default=True)
     started_at          = DateTimeField(index=True)
@@ -81,16 +83,25 @@ class Auction(AuctionDataModel):
     def json_obj2model_dict(cls, obj, ts):
         model_dict = {
             'started_at': ts,
-            'item_extra': json.dumps({k: v for k,v in obj.iteritems() if k not in cls.ITEM_EXTRA_IGNORE_KEYS})
+            'item_extra': json.dumps({k: v for k,v in obj.iteritems() \
+                    if k not in cls.ITEM_EXTRA_IGNORE_KEYS}) \
+                or None,
         }
         model_dict.update({k: obj[v] for k,v in cls.KEY_MAP.iteritems()})
         return model_dict
+
+    @property
+    def item_attributes(self):
+        if self.item_extra is not None:
+            return json.loads(self.item_extra)
+        else:
+            return None
 
 class Snapshot(AuctionDataModel):
     auction             = ForeignKeyField(Auction, related_name='snapshots')
     timestamp           = DateTimeField()
 
-    bid                 = IntegerField()
+    bid                 = IntegerField(constraints=[Check('bid > 0')])
     time_left           = CharField()
 
     @classmethod
@@ -101,6 +112,8 @@ class Snapshot(AuctionDataModel):
             'bid':          obj['bid'],
             'time_left':    obj['timeLeft'],
         }
+
+MODELS = [Auction, Snapshot]
 
 class DataSource(object):
     def __init__(self, path):
@@ -125,12 +138,12 @@ class DataSource(object):
         return sorted(glob.glob(os.path.join(self._path, '*.json.bz2')))
 
 class DataManager(object):
-    def run(self, data_src, batch_size=200):
+    def run(self, data_src, batch_size=50, day_buffer=7):
         # aids = Auction IDs
         for ts, data in data_src:
             active_aids = set(a.auc_id for a in Auction.select().where(
                 (Auction.is_active == True) &
-                (Auction.started_at >= (ts - datetime.timedelta(days=7))) &
+                (Auction.started_at >= (ts - datetime.timedelta(days=day_buffer))) &
                 (Auction.started_at <= ts)
             ))
             logger.debug('Found active auction IDs: %d', len(active_aids))
@@ -138,10 +151,10 @@ class DataManager(object):
             ended_aids = list(active_aids - set(a['auc'] for a in data['auctions']))
             logger.debug('Found ended auction IDs: %d', len(ended_aids))
             with DB_PROXY.atomic():
-                for i in range(0, len(ended_aids), batch_size):
+                for i in tqdm(range(0, len(ended_aids), batch_size)):
                     Auction.update(is_active=False, ended_at=ts).where(
                         (Auction.auc_id << ended_aids[i:i+batch_size]) & #probably over a size limit
-                        (Auction.started_at >= (ts - datetime.timedelta(days=7))) &
+                        (Auction.started_at >= (ts - datetime.timedelta(days=day_buffer))) &
                         (Auction.started_at <= ts)
                     ).execute()
             del ended_aids
@@ -149,7 +162,7 @@ class DataManager(object):
             new_auctions = [a for a in data['auctions'] if a['auc'] not in active_aids]
             logger.debug('Found new auctions: %d', len(new_auctions))
             with DB_PROXY.atomic():
-                for i in range(0, len(new_auctions), batch_size):
+                for i in tqdm(range(0, len(new_auctions), batch_size)):
                     Auction.insert_many(
                         [Auction.json_obj2model_dict(a, ts) for a in new_auctions[i:i+batch_size]]
                     ).execute()
@@ -160,7 +173,7 @@ class DataManager(object):
             del new_auctions
             logger.debug('Inserting new auction snapshots: %d', len(new_auction_models))
             with DB_PROXY.atomic():
-                for i in range(0, len(new_auction_models), batch_size):
+                for i in tqdm(range(0, len(new_auction_models), batch_size)):
                     Snapshot.insert_many(
                         [Snapshot.json_obj2model_dict(new_auction_models[aid], new_auction_objs[aid], ts) \
                             for aid in new_aids[i:i+batch_size]]
@@ -171,7 +184,7 @@ class DataManager(object):
             new_snapshots = [a for a in data['auctions'] if a['auc'] in active_aids]
             new_snapshot_models = {a.auc_id: a for a in Auction.select().where(
                 (Auction.is_active == True) &
-                (Auction.started_at >= (ts - datetime.timedelta(days=7))) &
+                (Auction.started_at >= (ts - datetime.timedelta(days=day_buffer))) &
                 (Auction.started_at < ts)
             )}
             new_snapshot_objs = {a['auc']: a for a in new_snapshots}
@@ -179,42 +192,28 @@ class DataManager(object):
             del new_snapshots
             logger.debug('Inserting old auction snapshots: %d', len(new_snapshot_models))
             with DB_PROXY.atomic():
-                for i in range(0, len(new_snapshot_models), batch_size):
+                for i in tqdm(range(0, len(new_snapshot_models), batch_size)):
                     Snapshot.insert_many(
                         [Snapshot.json_obj2model_dict(new_snapshot_models[aid], new_snapshot_objs[aid], ts) \
                             for aid in new_aids[i:i+batch_size]]
                     ).execute()
 
-    def build_auction_model(self, ts, auc):
-        item_extra = json.dumps({k: v for k,v in auc.iteritems() if k not in Auction.ITEM_EXTRA_IGNORE_KEYS})
-        auc = Auction(
-            item_extra=item_extra,
-            started_at=ts,
-            **{k: auc[v] for k,v in Auction.KEY_MAP.iteritems()}
-        )
-        return auc
-
-    def build_snapshot_model(self, ts, auc_data, auc_model):
-        snap = Snapshot(
-            auction=auc_model,
-            timestamp=ts,
-            bid=auc_data['bid'],
-            time_left=auc_data['timeLeft'],
-        )
-        return snap
-
 
 if __name__ == '__main__':
     import sys
+    import optparse
 
-    data_dir = sys.argv[1]
-    db_fn = sys.argv[2]
+    parser = optparse.OptionParser()
 
-    db = SqliteDatabase(db_fn)
-    DB_PROXY.initialize(db)
-    if not os.path.exists(db_fn):
-        db.create_tables([Auction, Snapshot])
+    parser.add_option('-b', '--batch-size', type='int', default=50)
+    parser.add_option('--day-buffer', type='int', default=7)
 
-    ds = DataSource(data_dir)
+    opts, args = parser.parse_args()
+    data_path, db_addr = args
+
+    DB_PROXY.initialize(db_connect(db_addr))
+    DB_PROXY.create_tables(MODELS, safe=True)
+
+    ds = DataSource(data_path)
     dm = DataManager()
-    dm.run(ds, batch_size=50)
+    dm.run(ds, batch_size=opts.batch_size, day_buffer=opts.day_buffer)
